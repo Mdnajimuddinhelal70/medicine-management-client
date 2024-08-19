a74f80555fe1d2912c810be0e16ed9fb8c9feb9b
najim77288 <najimuddin77288@gmail.com>
2024-08-19 21:03:53 +0600

    used tan stack query and get payment history from server

--- src/components/Invoice/InvoicePage.jsx
+++ src/components/Invoice/InvoicePage.jsx
@@ -3,6 +3,11 @@
 // import 'jspdf-autotable'; // For generating tables in PDF
 // import logo from '../../assets/research.png'; // Replace with your logo path
 
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+
 // const InvoicePage = ({ user, purchase }) => {
 //   const generatePDF = () => {
 //     const doc = new jsPDF();
@@ -74,11 +79,43 @@
 
 // export default InvoicePage;
 
-
 const InvoicePage = () => {
+  const { user } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments/${user?.email}`);
+      return res.data;
+    },
+  });
   return (
     <div>
-      
+      <h2 className="text-2xl">Total Payment History</h2>
+
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Price</th>
+              <th>Transaction Id</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {payments.map((payment, index) =>  <tr key={payment._id}>
+              <th>{index + 1}</th>
+              <td>{payment.price}</td>
+              <td>{payment.transactionId}</td>
+              <td>{payment.status}</td>
+            </tr>)}
+           
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
