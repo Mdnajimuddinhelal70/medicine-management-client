7f136bb2a8c0fd43c60f877c17540c2d33d16fa2
najim77288 <najimuddin77288@gmail.com>
2024-08-21 10:55:08 +0600

    get users from the server side to the dashborsd

--- src/pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,10 +1,58 @@
-
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from './../../../hooks/useAxiosSecure';
+import { FaUser } from 'react-icons/fa';
 
 const ManageUsers = () => {
+    const axiosSecure = useAxiosSecure();
+    
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure?.get('/users');
+            return res.data;
+        }
+    });
+
     return (
         <div>
-            <div className="h2-text-2xl">
-                Manage Users
+            <div className="flex justify-evenly my-4">
+                <h2 className="text-2xl">All users</h2>
+                <h2 className="text-2xl">Total users: {users.length}</h2>
+            </div>
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users?.map((user, index) => (
+                            <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <button onClick={() => handleMakeAdmin(users)}
+                                        className='btn btn-md bg-sky-800 text-white'
+                                        >
+                                            <FaUser
+                                            className='text-white text-xl'
+                                            ></FaUser>
+                                    </button>
+                                </td>
+                                <td>
+                                    
+                                </td>
+
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
